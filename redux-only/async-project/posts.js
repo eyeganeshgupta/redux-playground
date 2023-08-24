1b8c0e1758b87286bc092a1b1aca90a124623176
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-24 09:46:05 +0530

    using-third-party-middleware_redux-logger

--- redux-only/async-project/posts.js
+++ redux-only/async-project/posts.js
@@ -1,4 +1,5 @@
-const { createStore } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const loggerMiddleware = require("redux-logger").createLogger();
 
 // initial state
 const initialState = {
@@ -30,9 +31,26 @@ const fetchPostFailed = () => {
 };
 
 // reducers
-const postsReducer = (state = initialState, action) => {};
+const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_STARTED:
+      return {
+        posts: ["HTML"],
+      };
+
+    default:
+      return state;
+  }
+};
 
 // store
-const store = createStore(postsReducer);
+const store = createStore(postsReducer, applyMiddleware(loggerMiddleware));
+
+// subscribe
+store.subscribe(() => {
+  const data = store.getState();
+  console.log(data);
+});
 
 // dispatch
+store.dispatch(fetchPostRequest());
