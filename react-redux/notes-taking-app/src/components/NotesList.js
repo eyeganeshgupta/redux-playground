1ef7eec60e4b3ba168451f3c5d15ddfc417bd390
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-25 10:18:31 +0530

    notes-taking-app-starter-template

---
+++ react-redux/notes-taking-app/src/components/NotesList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NotesList.css";
+
+const NotesList = () => {
+  return (
+    <>
+      <h1>Notes List</h1>
+      <div key="g" className="item-container">
+        <div className="item-content">
+          <h2>some title</h2>
+          <p>some content</p>
+          <button>Delete</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotesList;
