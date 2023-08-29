ff09286629dae33bc104d2a0b29441bf905087e1
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-29 23:47:33 +0530

    search-all-posts-reducer

--- react-redux/posts-app/src/redux/reducers/postReducer.js
+++ react-redux/posts-app/src/redux/reducers/postReducer.js
@@ -14,3 +14,37 @@ const initialState = {
   post: {},
   error: "",
 };
+
+// reducers
+export const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // ==== fetch/search all posts
+    case FETCH_POSTS_REQUEST:
+      return {
+        loading: true,
+        posts: [],
+        post: {},
+        error: "",
+      };
+
+    case FETCH_POSTS_SUCCESS:
+      return {
+        loading: false,
+        posts: action.payload,
+        post: {},
+        error: "",
+      };
+
+    case FETCH_POSTS_ERROR:
+      return {
+        loading: false,
+        posts: [],
+        post: {},
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
